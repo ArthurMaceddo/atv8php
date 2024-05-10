fd90eb62ee0080f1f3d194bbcc0719a2a031b35d
Arthur <arthurmaceddo@gmail.com>
2024-05-09 22:56:29 -0300

    first commit

---
+++ resources/views/layouts/topo.blade.php
@@ -0,0 +1,18 @@
+<div class="header">
+    <ul>
+        <li>
+            <a href={{route('site.index')}}>
+                Incio
+            </a>
+            <a href={{route ('app.circulo')}}>
+                Circulo
+            </a>
+            <a href={{route ('app.retangulo')}}>
+                Retangulo
+            </a>
+            <a href={{route('app.quadrado')}}>
+                Quadrado
+            </a>
+        </li>
+    </ul>
+</div>
---
+++ resources/views/regras.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Regras para Cálculo de Área</title>
+</head>
+<body>
+    <h1>Regras para Cálculo de Área</h1>
+    <ul>
+        <li><a href="{{ route('calcula.area', 'quadrado') }}">Calcular Área do Quadrado</a></li>
+        <li><a href="{{ route('calcula.area', 'retangulo') }}">Calcular Área do Retângulo</a></li>
+        <li><a href="{{ route('calcula.area', 'circulo') }}">Calcular Área do Círculo</a></li>
+        
+    </ul>
+</body>
+</html>
